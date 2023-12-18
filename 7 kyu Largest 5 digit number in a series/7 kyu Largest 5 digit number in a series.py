4ae1b9528c5ae12cb558bb8cdb8d561defb11ef2
Edward Oganezov <eddyssintax@gmail.com>
2023-12-18 23:44:32 +0400

    Create 7 kyu Largest 5 digit number in a series.py

---
+++ 7 kyu Largest 5 digit number in a series/7 kyu Largest 5 digit number in a series.py
@@ -0,0 +1,22 @@
+'''In the following 6 digit number:
+
+283910
+91 is the greatest sequence of 2 consecutive digits.
+
+In the following 10 digit number:
+
+1234567890
+67890 is the greatest sequence of 5 consecutive digits.
+
+Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. 
+The number will be passed in as a string of only digits.
+ It should return a five digit integer. The number passed may be as large as 1000 digits.'''
+
+def solution(digits):
+    num = 0
+    for iter in range(len(digits)):
+        if int(digits[iter:iter + 5]) > num:
+            num = int(digits[iter:iter + 5])
+    return num
+        
+    
